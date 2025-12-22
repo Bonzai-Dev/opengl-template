05c5d8e0b2f918cdc661839777d9e2aff6533620
Bonzai-Dev <137844567+Bonzai-Dev@users.noreply.github.com>
2025-12-22 12:57:29 -0500

    Added template

---
+++ src/core/window.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "logger.hpp"
+#include <string_view>
+#include <SDL3/SDL.h>
+
+class Window {
+  public:
+    Window(std::string_view windowName);
+
+    void Destroy() const;
+
+    void UpdateBuffer() const;
+
+  private:
+    SDL_GLContext m_glContext;
+    SDL_Window* m_sdlWindow;
+    SDL_DisplayMode m_displayMode;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
-#include <stb_image.h>
-#include <glad/glad.h>
-#include <stdio.h>
+#include <glad/gl.h>
+#include <SDL3/SDL.h>
+#include <core/logger.hpp>
+#include <core/window.hpp>
 
 int main() {
-  std::cout << "Starting image load test..." << std::endl;
+  SDL_Init(SDL_INIT_VIDEO);
+
+  const auto window = Window("OpenGL 4.6 core profile");
+
+  int exit = 0;
+  while (!exit) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_EVENT_QUIT:
+          exit = 1;
+          break;
+        case SDL_EVENT_KEY_UP:
+          if (event.key.key == SDLK_ESCAPE) {
+            exit = 1;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    window.UpdateBuffer();
+  }
+
+  window.Destroy();
+  SDL_Quit();
+
   return 0;
 }
